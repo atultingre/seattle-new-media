724f1fdc5b7032cf9a5d6a2736b9c905e813197d
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-21 11:44:08 +0530

    Inital App Setup

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      LandingPage
+      {/* Implement isLogged in setup route Logic Login / Dashboard */}
+    </div>
+  );
+};
+
+export default LandingPage;
