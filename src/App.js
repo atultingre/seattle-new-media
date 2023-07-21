f202839cb57da3fca82f2d204d82dbc6b29ac401
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-21 11:56:53 +0530

    Inital App Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import LoginPage from "./components/LoginPage";
+import DashboardPage from "./components/DashboardPage";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      App
-      {/* Implement Routing */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
