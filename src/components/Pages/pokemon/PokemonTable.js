a5805bab9e50c2a888e90a628644639581ad7797
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-23 05:47:21 +0530

    pokemon code splitting

---
+++ src/components/Pages/pokemon/PokemonTable.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Table, Typography, } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
+
+const PokemonTable = ({ pokemonList, loading, currentPage, itemsPerPage, totalResults, onTableChange, onViewDetails }) => {
+  const columns = [
+    {
+      title: "Sr No",
+      dataIndex: "srno",
+      key: "srno",
+      render: (text, record, index) =>
+        (currentPage - 1) * itemsPerPage + index + 1,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => <Typography>{name}</Typography>,
+    },
+    {
+      title: "Details",
+      dataIndex: "url",
+      key: "url",
+      render: (url, record) => (
+        <span onClick={() => onViewDetails(record)}>
+          <EyeOutlined />
+        </span>
+      ),
+    },
+  ];
+
+  const paginationConfig = {
+    current: currentPage,
+    pageSize: itemsPerPage,
+    total: totalResults,
+    onChange: onTableChange,
+  };
+
+  return (
+    <Table
+      size="large"
+      columns={columns}
+      dataSource={pokemonList.map((pokemon, index) => ({
+        ...pokemon,
+        key: index.toString(),
+      }))}
+      pagination={paginationConfig}
+      loading={loading}
+    />
+  );
+};
+
+export default PokemonTable;
